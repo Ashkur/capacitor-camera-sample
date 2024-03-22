7cc46e53ccb25ab0d7328c2a097efb903d728db7
Uriel Carneiro <usilva@esapiens.com.br>
2024-03-22 10:09:10 -0300

    feat: 🎸 first commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,69 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonText,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react'
+import './Home.css'
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera'
 
 const Home: React.FC = () => {
+  const onGetPhoto = async () => {
+    try {
+      const photoResult = await Camera.getPhoto({
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Camera,
+        quality: 100,
+      })
+
+      console.log(photoResult)
+    } catch (error) {
+      console.log(error) // no logs are printed
+    }
+  }
+
+  const OpenCamera = async () => {
+    try {
+      const { camera: cameraPermission } = await Camera.checkPermissions()
+
+      if (cameraPermission === 'granted') {
+        onGetPhoto()
+      } else {
+        const { camera: NewCameraPermission } = await Camera.requestPermissions(
+          { permissions: ['camera'] }
+        )
+
+        if (NewCameraPermission === 'granted') {
+          onGetPhoto()
+        }
+      }
+    } catch (error) {
+      console.log(error) // no logs are printed
+    }
+  }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>CAMERA SAMPLE</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
+        <IonHeader collapse='condense'>
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size='large'>Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <IonButton onClick={OpenCamera}>
+          <IonText>OPEN CAMERA</IonText>
+        </IonButton>
       </IonContent>
     </IonPage>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
